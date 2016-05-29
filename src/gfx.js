3874ba86981853f651a97271c992f716d74df641
Stephen Niedzielski <stephen@niedzielski.com>
2016-05-29 16:20:59 -0600

    Fix infinite loop when drawing a Bresenham line
    When passed noninteger arguments, the Bresenham line drawing algorithm
    may spin forever. Sanitize the arguments before using them.

--- src/gfx.js
+++ src/gfx.js
@@ -6,24 +6,29 @@
     @return {void}
     @see http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#JavaScript */ // eslint-disable-line max-len
 function bline(gfx, p0, p1) {
-  const dx = Math.abs(p1.x - p0.x)
-  const dy = Math.abs(p1.y - p0.y)
-  const sx = p0.x < p1.x ? 1 : -1
-  const sy = p0.y < p1.y ? 1 : -1
+  let x0 = Math.round(p0.x)
+  let y0 = Math.round(p0.y)
+  const x1 = Math.round(p1.x)
+  const y1 = Math.round(p1.y)
+
+  const dx = Math.abs(x1 - x0)
+  const dy = Math.abs(y1 - y0)
+  const sx = x0 < x1 ? 1 : -1
+  const sy = y0 < y1 ? 1 : -1
   let err = (dx > dy ? dx : -dy) / 2
 
-  pixel(gfx, p0.x, p0.y)
-  while (p0.x !== p1.x || p0.y !== p1.y) {
+  pixel(gfx, x0, y0)
+  while (x0 !== x1 || y0 !== y1) {
     const prevErr = err
     if (prevErr > -dx) {
       err -= dy
-      p0.x += sx
+      x0 += sx
     }
     if (prevErr < dy) {
       err += dx
-      p0.y += sy
+      y0 += sy
     }
-    pixel(gfx, p0.x, p0.y)
+    pixel(gfx, x0, y0)
   }
 }
 module.exports.bline = bline
