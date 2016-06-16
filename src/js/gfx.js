e7e51636197f57ade582dccf41a4eef1da377f03
Stephen Niedzielski <stephen@niedzielski.com>
2016-06-15 23:05:28 -0600

    Add Flow and delint

--- src/js/gfx.js
+++ src/js/gfx.js
@@ -1,27 +1,30 @@
+// @flow
+
+const Phaser = require('./phaser')
+
 /**
  * Phaser.Graphics.lineTo seems to be better suited for antialiased lines. Use a
  * classic Bresenham line instead.
- * @arg {Phaser.Graphics} gfx
- * @arg {Phaser.Point} p0
- * @arg {Phaser.Point} p1
- * @return {void}
  * @see http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#JavaScript
  */
-function bline(gfx, p0, p1) {
-  let x0 = Math.floor(p0.x)
-  let y0 = Math.floor(p0.y)
-  const x1 = Math.floor(p1.x)
-  const y1 = Math.floor(p1.y)
+function bline(gfx: Phaser.Graphics, p0: Phaser.Point, p1: Phaser.Point): void {
+  let
+    x0: number = Math.floor(p0.x),
+    y0: number = Math.floor(p0.y)
+  const
+    x1: number = Math.floor(p1.x),
+    y1: number = Math.floor(p1.y)
 
-  const dx = Math.abs(x1 - x0)
-  const dy = Math.abs(y1 - y0)
-  const sx = x0 < x1 ? 1 : -1
-  const sy = y0 < y1 ? 1 : -1
-  let err = (dx > dy ? dx : -dy) / 2
+  const // eslint-disable-line one-var
+    dx: number = Math.abs(x1 - x0),
+    dy: number = Math.abs(y1 - y0),
+    sx: number = x0 < x1 ? 1 : -1,
+    sy: number = y0 < y1 ? 1 : -1
+  let err: number = (dx > dy ? dx : -dy) / 2 // eslint-disable-line one-var
 
   pixel(gfx, x0, y0)
   while (x0 !== x1 || y0 !== y1) {
-    const prevErr = err
+    const prevErr: number = err
     if (prevErr > -dx) {
       err -= dy
       x0 += sx
@@ -33,20 +36,17 @@ function bline(gfx, p0, p1) {
     pixel(gfx, x0, y0)
   }
 }
-module.exports.bline = bline
 
-/**
- * @arg {Phaser.Graphics} gfx
- * @arg {number} x
- * @arg {number} y
- * @return {void}
- */
-function pixel(gfx, x, y) {
+function pixel(gfx: Phaser.Graphics, x: number, y: number): void {
   // Phaser / PIXI doesn't expose a set pixel method and Canvas / WebGL seem to
   // try to antialias on boundaries even with
   // Phaser.Canvas.setImageRenderingCrisp enabled. Use a tiny width and diameter
   // so the chance of bleeding into other pixels is small.
-  const diameter = .01
+  const diameter: number = .01
   gfx.drawCircle(x, y, diameter)
 }
-module.exports.pixel = pixel
+
+module.exports = {
+  bline,
+  pixel
+}
